5897a64ef0e1c6957ac8d8c02f18b900198afeb6
Alp Sarıkışla <alpsarikisla@hotmail.com>
2024-11-17 11:31:28 +0300

    Login Complated

--- FishToolsStoreECommerceApp/Areas/ManagerPanel/Controllers/LoginController.cs
+++ FishToolsStoreECommerceApp/Areas/ManagerPanel/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FishToolsStoreECommerceApp.Areas.ManagerPanel.Data.ViewModels;
+using FishToolsStoreECommerceApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,36 @@ namespace FishToolsStoreECommerceApp.Areas.ManagerPanel.Controllers
 {
     public class LoginController : Controller
     {
+        FishToolsStoreModel db = new FishToolsStoreModel();
         // GET: ManagerPanel/Login
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Index(ManagerLoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Manager m = db.Managers.FirstOrDefault(x => x.Mail == model.Mail && x.Password == model.Password);
+                if (m != null)
+                {
+                    if (m.IsActive)
+                    {
+                        Session["manager"] = m;
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ViewBag.warning = "Kullanıcı hesabınız sistem yöneticisi tarafından askıya alınmıştır";
+                    }
+                }
+                else
+                {
+                    ViewBag.warning = "Kullanıcı bulunamadı";
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ FishToolsStoreECommerceApp/Areas/ManagerPanel/Data/ViewModels/ManagerLoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FishToolsStoreECommerceApp.Areas.ManagerPanel.Data.ViewModels
+{
+    public class ManagerLoginViewModel
+    {
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage ="Mail alanı zorunludur")]
+        public string Mail { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Şifre alanı zorunludur")]
+        public string Password { get; set; }
+    }
+}
