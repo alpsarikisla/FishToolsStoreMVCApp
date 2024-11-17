7e720fa077c22e8946fa9c22a44232a8a4de499f
Alp Sarıkışla <alpsarikisla@hotmail.com>
2024-11-17 10:27:38 +0300

    Login Page And Style

---
+++ FishToolsStoreECommerceApp/Areas/ManagerPanel/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FishToolsStoreECommerceApp.Areas.ManagerPanel.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: ManagerPanel/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FishToolsStoreECommerceApp/Areas/ManagerPanel/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FishToolsStoreECommerceApp.Areas.ManagerPanel.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: ManagerPanel/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
