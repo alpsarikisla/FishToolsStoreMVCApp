1ebf38a7c811d39bec26c5c71e05aaa1a252fbde
Alp Sarıkışla <alpsarikisla@hotmail.com>
2024-11-23 12:58:50 +0300

    Marka Ekleme Listeleme Tamamlandı

---
+++ FishToolsStoreECommerceApp/Areas/ManagerPanel/Controllers/BrandsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FishToolsStoreECommerceApp.Models;
+
+namespace FishToolsStoreECommerceApp.Areas.ManagerPanel.Controllers
+{
+    public class BrandsController : Controller
+    {
+        private FishToolsStoreModel db = new FishToolsStoreModel();
+
+        // GET: ManagerPanel/Brands
+        public ActionResult Index()
+        {
+            return View(db.Brands.ToList());
+        }
+
+        // GET: ManagerPanel/Brands/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Brand brand = db.Brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+
+        // GET: ManagerPanel/Brands/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ManagerPanel/Brands/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,IsActive,IsDeleted")] Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Brands.Add(brand);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(brand);
+        }
+
+        // GET: ManagerPanel/Brands/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Brand brand = db.Brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+
+        // POST: ManagerPanel/Brands/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,IsActive,IsDeleted")] Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(brand).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(brand);
+        }
+
+        // GET: ManagerPanel/Brands/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Brand brand = db.Brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+
+        // POST: ManagerPanel/Brands/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Brand brand = db.Brands.Find(id);
+            db.Brands.Remove(brand);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
