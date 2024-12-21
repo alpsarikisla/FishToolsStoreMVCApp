de050e60312201eafe783121a1d2f15273021c07
Alp Sarıkışla <alpsarikisla@hotmail.com>
2024-12-21 11:20:53 +0300

    proje Birleştirildi

---
+++ FishToolsStoreECommerceApp/Controllers/MemberController.cs
@@ -0,0 +1,92 @@
+﻿using FishToolsStoreECommerceApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FishToolsStoreECommerceApp.Controllers
+{
+    public class MemberController : Controller
+    {
+        FishToolsStoreModel db = new FishToolsStoreModel();
+        // GET: Member
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Edit(int? id)
+        {
+
+            Member user = Session["user"] as Member;
+
+            if (user == null)
+            {
+                ViewBag.Warning = "Kullanıcı oturumunda bir hata oluştu.";
+                return RedirectToAction("Index", "Home");
+            }
+
+            Member member = db.Members.Find(user.ID);
+
+            if (member != null)
+            {
+                return View(member);
+            }
+            else
+            {
+                ViewBag.Warning = "Kullanıcı bulunamadı.";
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Member user)
+        {
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Member member = db.Members.Find(user.ID);
+
+                    if (member != null)
+                    {
+                        member.Name = user.Name;
+                        member.Surname = user.Surname;
+                        member.UserName = user.UserName;
+                        member.Mail = user.Mail;
+                        member.Password = user.Password;
+
+                        db.Entry(member).State = EntityState.Modified;
+
+                        if (db.SaveChanges() > 0)
+                        {
+                            Session["user"] = member;
+                            ViewBag.Success = "Profil başarıyla güncellendi.";
+                        }
+                        else
+                        {
+                            ViewBag.Warning = "Veritabanı güncellemesi sırasında bir sorun oluştu.";
+                        }
+                    }
+                    else
+                    {
+                        ViewBag.Warning = "Kullanıcı bulunamadı.";
+                    }
+                }
+                catch
+                {
+                    ViewBag.Warning = "Bir hata oluştu. Lütfen tekrar deneyin.";
+                }
+            }
+            else
+            {
+                ViewBag.Warning = "Girilen bilgilerde hata var. Lütfen kontrol edip tekrar deneyin.";
+            }
+
+            return View(user);
+        }
+    }
+}
--- FishToolsStoreECommerceApp/Controllers/ShoppingCartController.cs
+++ FishToolsStoreECommerceApp/Controllers/ShoppingCartController.cs
@@ -1,6 +1,7 @@
 ﻿using FishToolsStoreECommerceApp.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,46 +16,130 @@ namespace FishToolsStoreECommerceApp.Controllers
             if (Session["user"] != null)
             {
                 int id = (Session["user"] as Member).ID;
-                return View(db.ShoppingCarts.Where(s => s.Member_ID == id).ToList());
+
+                List<ShoppingCart> cart = db.ShoppingCarts
+                                        .Where(s => s.Member_ID == id)
+                                        .Include(s => s.Product)
+                                        .ToList();
+
+                return View(cart);
             }
             else
             {
                 return RedirectToAction("Index", "Login");
             }
         }
-        [HttpPost]
-        public ActionResult AddToCart(int? id, int quantity)
+
+        public ActionResult AddToCart(int id, int quantity)
         {
             if (Session["user"] != null)
             {
-                ShoppingCart sc = new ShoppingCart();
-                sc.Member_ID = (Session["user"] as Member).ID;
-                sc.Product_ID = Convert.ToInt32(id);
-                sc.Quantity = quantity;
-                db.ShoppingCarts.Add(sc);
+                int memberId = (Session["user"] as Member).ID;
+
+                Product product = db.Products.Find(id); 
+                if (product == null || product.Stock < quantity) 
+                {
+                    ViewBag.Error = $"Üzgünüz, '{product?.Name}' ürünü için yeterli stok yok.";
+                    return View("Index", db.ShoppingCarts.Where(s => s.Member_ID == memberId).Include(s => s.Product).ToList());
+                }
+
+                ShoppingCart cart = db.ShoppingCarts.FirstOrDefault(s => s.Member_ID == memberId && s.Product_ID == id);
+
+                if (cart != null)
+                {
+                    if (product.Stock < cart.Quantity + quantity)
+                    {
+                        ViewBag.Error = $"Üzgünüz, '{product.Name}' ürünü için bu kadar stok mevcut değil.";
+                        return View("Index", db.ShoppingCarts.Where(s => s.Member_ID == memberId).Include(s => s.Product).ToList());
+                    }
+                    cart.Quantity += quantity;
+                }
+                else
+                {
+                    ShoppingCart sc = new ShoppingCart
+                    {
+                        Member_ID = memberId,
+                        Product_ID = id,
+                        Quantity = quantity
+                    };
+                    db.ShoppingCarts.Add(sc);
+                }
+
                 db.SaveChanges();
             }
             else
             {
                 return RedirectToAction("Index", "Login");
             }
+
             return RedirectToAction("Index", "ShoppingCart");
         }
-        public ActionResult DeleteFromCart(int id)
+        public ActionResult UpdateQuantity(int productId, int quantity)
         {
             if (Session["user"] != null)
             {
-                ShoppingCart sc = db.ShoppingCarts.Find(id);
-                db.ShoppingCarts.Remove(sc);
-                db.SaveChanges();
+                int memberId = (Session["user"] as Member).ID;
+
+                ShoppingCart cart = db.ShoppingCarts.FirstOrDefault(s => s.Member_ID == memberId && s.Product_ID == productId);
+
+                if (cart != null)
+                {
+                    if (quantity > 0)
+                    {
+                        cart.Quantity = quantity;
+                        db.SaveChanges();
+                    }
+                }
             }
-            else
-            {
-                return RedirectToAction("Index", "Login");
-            }
-            return RedirectToAction("Index", "ShoppingCart");
+            return RedirectToAction("Index");
         }
 
+        public ActionResult ChangeQuantity(int productId, bool increase)
+        {
+            if (Session["user"] != null)
+            {
+                int memberId = (Session["user"] as Member).ID;
 
+                ShoppingCart cart = db.ShoppingCarts.FirstOrDefault(s => s.Member_ID == memberId && s.Product_ID == productId);
+                Product product = db.Products.Find(productId); 
+
+                if (cart != null && product != null)
+                {
+                    if (increase)
+                    {
+                        if (cart.Quantity + 1 > product.Stock) 
+                        {
+                            ViewBag.Error = $"Üzgünüz, '{product.Name}' ürünü için yeterli stok yok.";
+                            return View("Index", db.ShoppingCarts.Where(s => s.Member_ID == memberId).Include(s => s.Product).ToList());
+                        }
+                        cart.Quantity++;
+                    }
+                    else if (cart.Quantity > 1)
+                    {
+                        cart.Quantity--;
+                    }
+
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteCart(int productId)
+        {
+            if (Session["user"] != null)
+            {
+                int memberId = (Session["user"] as Member).ID;
+
+                ShoppingCart cart = db.ShoppingCarts.FirstOrDefault(s => s.Member_ID == memberId && s.Product_ID == productId);
+
+                if (cart != null)
+                {
+                    db.ShoppingCarts.Remove(cart);
+                    db.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
